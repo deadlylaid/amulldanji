da8cc7276429752b684a431e967b3d3c2396bc03
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-06 20:26:48 +0900

    update : login, logout > home.html feature login, logout

--- amulldanji/amulldanji/urls.py
+++ amulldanji/amulldanji/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from amulldanji.views import HomeView
-from users.views import JoinusView
+from users.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^joinus', JoinusView.as_view(), name='joinus'),
 
+    url(r'^joinus', JoinusView.as_view(), name='joinus'),
+    url(r'^login', LoginView.as_view(), name='login'),
+    url(r'^logout', LogoutView.as_view(), name='logout'),
 ]
---
+++ amulldanji/users/views/logout.py
@@ -0,0 +1,12 @@
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, get_user_model
+from django.core.urlresolvers import reverse
+
+
+class LogoutView(View):
+    
+    def get(self, request):
+        logout(request)
+        return redirect(reverse("home"))
+
