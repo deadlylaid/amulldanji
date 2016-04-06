868027006853bf0a70faecd78ef7e01e0b88ac8d
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-06 11:44:16 +0900

    update : joinus templates home templates

--- amulldanji/users/views/joinus.py
+++ amulldanji/users/views/joinus.py
@@ -1,10 +1,12 @@
 from django.views.generic import View
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 class JoinusView(View):
 
     def get(self, request):
-        return HttpResponse(
-                "hello world",
+        return render(
+                request,
+                "users/joinus.html",
+                context={}
                 )
