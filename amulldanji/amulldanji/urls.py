677cd885647267607ecac9141f7b96b48b5b19c0
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-07 17:54:27 +0900

    update : item detail, list view

--- amulldanji/amulldanji/urls.py
+++ amulldanji/amulldanji/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
 
-    url(r'^joinus', JoinusView.as_view(), name='joinus'),
-    url(r'^login', LoginView.as_view(), name='login'),
-    url(r'^logout', LogoutView.as_view(), name='logout'),
+    url(r'^joinus/$', JoinusView.as_view(), name='joinus'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
-    url(r'^itemlist', ItemListView.as_view(), name='itemlist'),
+    url(r'^item/$', ItemListView.as_view(), name='itemlist'),
     url(r'^item/(?P<pk>\d+)/$', ItemDeatilView.as_view(), name='itemdetail'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
