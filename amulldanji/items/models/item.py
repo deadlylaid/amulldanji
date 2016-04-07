ba30fdcbc304afad73ba357f095c8d792940d799
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-07 11:07:42 +0900

    update : Item class(user,title,content)

--- amulldanji/items/models/item.py
+++ amulldanji/items/models/item.py
@@ -1,7 +1,12 @@
 from django.db import models
-
+from users.models import User
 
 class Item(models.Model):
+
+    user = models.ForeignKey(
+            User,
+            related_name='items',
+            )
 
     title = models.CharField(
             max_length=50,
@@ -10,4 +15,4 @@ class Item(models.Model):
     content = models.TextField()
 
     def __str__(self):
-        return self.title()
+        return self.title
