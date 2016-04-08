675006807e2b3409463ffa1f4d81f816b96fa181
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-08 16:25:02 +0900

    update : insert img 'a' tag and url path in home.html

--- amulldanji/amulldanji/urls.py
+++ amulldanji/amulldanji/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^joinus/$', JoinusView.as_view(), name='joinus'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
-    url(r'^mypage/(?P<pk>\d+)/$', MyPageView.as_view(), name='itemdetail'),
+    url(r'^mypage/(?P<pk>\d+)/$', MyPageView.as_view(), name='mypage'),
 
     url(r'^item/(?P<pk>\d+)/$', ItemDeatilView.as_view(), name='itemdetail'),
     url(r'^item/(?P<pk>\d+)/comments/$', CommentCreateView.as_view(), name='item-comments'),
