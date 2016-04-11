38e9b40aa6da6351f5a0a0a32e45e46bbf65165e
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-11 15:30:28 +0900

    update : update front-end design

--- amulldanji/amulldanji/static/js/application.js
+++ amulldanji/amulldanji/static/js/application.js
@@ -4,7 +4,6 @@
         var $transfer = $('#transfer');
         
         $('#transfer').on('click',function(event){
-            event.preventDefault();
             if (transfer_click <= 0){
                 alert("현재 보유 중이신 티켓의 갯수가 부족하여 양도 받으실 수 없습니다.");
             }
