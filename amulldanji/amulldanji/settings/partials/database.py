932794e6b7355ad6f19ee74eb4ea206e47c9e17e
hanminsoo <hanminsoo@hanminsooui-MacBook-Pro.local>
2016-04-12 15:10:32 +0900

    update : postgresql

--- amulldanji/amulldanji/settings/partials/database.py
+++ amulldanji/amulldanji/settings/partials/database.py
@@ -7,7 +7,9 @@ from .base import BASE_DIR
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
     }
 }
